54c49102fb20d84b5e67e63554028c475cec89bd
Matthieu Martinez Espino <matthieu.martinez@ecole-isitech.fr>
2023-01-24 11:50:45 +0100

    ommit du matin

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const List = [
+  {
+    Date: "August 2020 14",
+    Name: "Toilet Paper",
+    Price: "$94,12",
+  },
+
+  {
+    Date: "March 2021 12",
+    Name: "New TV",
+    Price: "$799,49",
+  },
+
+  {
+    Date: "March 2021 28",
+    Name: "Car Insurance",
+    Price: "$294.67",
+  },
+
+  {
+    Date: "June 2021 12",
+    Name: "New Desk (Wooden)",
+    Price: "$450",
+  },
+  ]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <p>{List.map((el)=>(
+        el.Date
+      ))}</p>
+
+      <p>{List.map((el)=>(
+        el.Name
+      ))}</p>
+
+      <p>{List.map((el)=>(
+        el.Price
+      ))}</p>
     </div>
   );
 }
